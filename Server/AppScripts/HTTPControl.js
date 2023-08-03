52710493b00aeba8dd5fc537ccf8aa68f62d109c
Harry Boyd <hboydemail@gmail.com>
2023-08-03 15:07:44 +0100

    Separated Json Control into its own file

--- Server/AppScripts/HTTPControl.js
+++ Server/AppScripts/HTTPControl.js
@@ -157,21 +157,3 @@ function doPost(event) {
     return createJsonOutput({ status: "error", message: error.message });
   }
 }
-
-/**
- * Creates a JSON formatted text output using the Google Apps Script
- * ContentService.
- *
- * The function accepts an object, stringifies it into a JSON format,
- * and creates a text output using the ContentService.
- * It sets the MIME type of the output to JSON.
- *
- * @param {Object} data - The data object to be converted into JSON.
- * @returns {TextOutput} A TextOutput object representing the HTTP response
- * in JSON format.
- */
-function createJsonOutput(data) {
-  return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(
-    ContentService.MimeType.JSON
-  );
-}
