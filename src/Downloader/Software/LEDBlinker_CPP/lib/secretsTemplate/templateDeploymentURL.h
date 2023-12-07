3f9c5c995efc8d68edfb796db1b7a2dd51478ecc
Harry Boyd <hboyd255@gmail.com>
2023-12-07 21:32:33 +0000

    Created Template Secret Data

---
+++ src/Downloader/Software/LEDBlinker_CPP/lib/secretsTemplate/templateDeploymentURL.h
@@ -0,0 +1,8 @@
+#ifndef TEMPLATE_DEPLOYMENT_URL_H
+#define TEMPLATE_DEPLOYMENT_URL_H
+
+// The URL to retrieve the data from, swap out DEPLOYMENT_ID with the deployment
+// ID obtained from the Google Apps Script deployment.
+#define SECRET_URL "https://script.google.com/macros/s/DEPLOYMENT_ID/exec?selector=lowest&count=5"
+
+#endif  // TEMPLATE_DEPLOYMENT_URL_H
