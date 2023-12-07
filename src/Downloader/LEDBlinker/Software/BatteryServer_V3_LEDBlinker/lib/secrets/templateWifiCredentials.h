94db20e9d761ddf6baa204a95e72456677711e9e
Harry Boyd <hboyd255@gmail.com>
2023-12-07 21:26:59 +0000

    Moved

--- src/Downloader/LEDBlinker/Software/BatteryServer_V3_LEDBlinker/lib/secrets/templateWifiCredentials.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEMPLATE_WIFI_CREDENTIALS_H
-#define TEMPLATE_WIFI_CREDENTIALS_H
-
-#define SSID "Your network name"
-#define WIFI_PASSWORD "your wifi password"
-
-#endif  // TEMPLATE_WIFI_CREDENTIALS_H
